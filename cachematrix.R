e00364acaa26d47464284a8a8c448116767a1147
Ram Marthi <ram.marthi@gmail.com>
2015-07-25 12:47:38 +0530

    MakeCacheMatrix & CacheSolve Functions
    MakeCacheMatrix & CacheSolve Functions are successfully programmed and
    presented for your reference and evaluation.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## MakeCacheMatrix has the following 
+## functions that creates a vector of  Matrix class. 
+## Sets the value of the vector and gets the same . 
+## Similarly the inverse value of the matrix is also created as setinverse.
+## And vector value is called back by calling getinverse.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## maieCacheMatrix creates a matrix to set, get, setinverse and getinverse 
+## the matrix value facilitating to call the getinverse of a function.
+makeCacheMatrix <- function(r = matrix()) 
+{
+      k <- NULL
+    set <- function(s) {
+      r <<- s
+      k <<- NULL
+    }
+    get <- function() r
+    setinverse <- function(inv) k <<- inv
+    getinverse <- function() k
+    list (set = set,
+          get = get,
+          setinverse = setinverse,
+          getinverse = getinverse)
 }
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Calculate the inverse of the special "matrix" created with the above
+## function, reusing cached result if it is available
+cacheSolve <- function(r, ...) 
+{
+  k <- r$getinverse()
+  if(!is.null(k)) {
+    message("getting cached data")
+    return(k)
+  }
+  m <- r$get()
+  k <- solve(m, ...)
+  r$setinverse(k)
+  k
 }
+## Here is an example showing us the inverse of a matrix Cached for a unit matrix.
+## Unit Matrix is choosen as i = i inverse, easy to check.
+u <- matrix(c(1,0,0,0,1,0,0,0,1),nrow=3,ncol=3)
+u
+v <- makeCacheMatrix(u)
+cacheSolve(v)
